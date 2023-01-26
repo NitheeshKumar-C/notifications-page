804fb3c9642aa3c7cc17cbe125484744159f3e69
NitheeshKumar-C <cnitheeshkumar20@gmail.com>
2023-01-26 21:57:54 +0530

    Updated Js code

--- Js/script.js
+++ Js/script.js
@@ -4,16 +4,11 @@ document.addEventListener('click', (e) => {
         if(element.className == "user" || element.className == "action"){        
             var pe=((element.parentElement).parentElement).parentElement;
             pe.style.backgroundColor= "hsl(0, 0%, 100%)";
-            pe.getElementsByClassName("dot")[0].style.display="none";
-            var a=document.getElementsByClassName("chat-container");
-                for(let i=0;i<a.length;i++){
-                    if(a[i].style.backgroundColor=="rgb(255, 255, 255)"){
-                        count--;
-                    }
-                }
-                if((count+3) >= 0){
-                    document.getElementById("msg-count").innerHTML=(count+3);
-                    count=0;
+            var ele=pe.getElementsByClassName("dot");
+            ele[0].parentNode.removeChild(ele[0]);
+                count = document.getElementsByClassName("dot").length;
+                if(count>= 0){
+                    document.getElementById("msg-count").innerHTML=(count);
                 }
                 
             
